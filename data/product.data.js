3baa0aded73c053f5b6e5d8e6b763b4909e458f3
Thanh Duy <thanhduy1278908@gmail.com>
2026-03-11 14:21:41 +0700

    tao trang chi tiet sp

--- data/product.data.js
+++ data/product.data.js
@@ -3,19 +3,43 @@ const products = [
     id: 1,
     name: "Rose",
     price: 10,
-    description: "Beautiful red rose flower"
+    description: "Beautiful red rose flower",
+    image: "/images/templatemo_image_01.jpg"
   },
   {
     id: 2,
     name: "Tulip",
     price: 12,
-    description: "Fresh tulip from garden"
+    description: "Fresh tulip from garden",
+    image: "/images/templatemo_image_02.jpg"
   },
   {
     id: 3,
     name: "Lily",
     price: 15,
-    description: "White lily flower"
+    description: "White lily flower",
+    image: "/images/templatemo_image_03.jpg"
+  },
+  {
+    id: 4,
+    name: "Sunflower",
+    price: 18,
+    description: "Bright sunflower bouquet for special days",
+    image: "/images/templatemo_image_04.jpg"
+  },
+  {
+    id: 5,
+    name: "Orchid",
+    price: 22,
+    description: "Elegant orchid arrangement in modern style",
+    image: "/images/templatemo_image_02.jpg"
+  },
+  {
+    id: 6,
+    name: "Daisy",
+    price: 9,
+    description: "Simple and fresh daisy bunch",
+    image: "/images/templatemo_image_01.jpg"
   }
 ];
 
